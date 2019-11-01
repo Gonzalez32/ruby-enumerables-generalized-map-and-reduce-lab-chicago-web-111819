70ba0ebd43657f8d3bb44ec36df8a93439414600
Juan Gonzalez <3232rose@gmail.com>
2019-11-01 16:49:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,3 +9,15 @@ def map(g)
    return array
 end
 
+def reduce(array, starting_point = 0)
+
+   array.each do |element|
+    starting_point = yield(starting_point, element)
+  end
+
+   if starting_point == 0
+    return true
+  end
+
+   return starting_point
+end
